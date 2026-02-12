7f714da4465f39816d91e3b5ebce6bc417c99e71
diablo <solsticeveil46@gmail.com>
2026-02-12 10:39:01 +0100

    feat(sbtc): Add withdrawal component 11

--- frontend/src/lib/sbtc-withdrawal.ts
+++ frontend/src/lib/sbtc-withdrawal.ts
@@ -24,3 +24,5 @@ export const WITHDRAWAL_STEP_8 = 'step_8';
 export const WITHDRAWAL_STEP_9 = 'step_9';
 
 export const WITHDRAWAL_STEP_10 = 'step_10';
+
+export const WITHDRAWAL_STEP_11 = 'step_11';
